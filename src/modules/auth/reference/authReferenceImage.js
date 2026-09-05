fc1dc3babe6a4e05c8dd34ee73581d8e48e9996f
ExpoProffsenter <firmapost@ringside.no>
2026-09-05 11:18:03 +0200

    Fase 36A1: samle valgt referansebilde

---
+++ src/modules/auth/reference/authReferenceImage.js
@@ -0,0 +1,8 @@
+import p1 from './part1.js';
+import p2 from './part2.js';
+import p3 from './part3.js';
+import p4 from './part4.js';
+import p5 from './part5.js';
+import p6 from './part6.js';
+
+export const AUTH_REFERENCE_IMAGE = `data:image/webp;base64,${p1}${p2}${p3}${p4}${p5}${p6}`;
