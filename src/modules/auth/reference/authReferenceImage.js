b6f038dbab719a55db831d0d89b6c77661b83203
ExpoProffsenter <firmapost@ringside.no>
2026-09-05 16:26:33 +0200

    Fase 36A1: restore fungerende seksdelt referansebilde

--- src/modules/auth/reference/authReferenceImage.js
+++ src/modules/auth/reference/authReferenceImage.js
@@ -1,3 +1,8 @@
-import cleanImage from './cleanImage.js';
+import p1 from './part1.js';
+import p2 from './part2.js';
+import p3 from './part3.js';
+import p4 from './part4.js';
+import p5 from './part5.js';
+import p6 from './part6.js';
 
-export const AUTH_REFERENCE_IMAGE = `data:image/webp;base64,${cleanImage}`;
+export const AUTH_REFERENCE_IMAGE = `data:image/webp;base64,${p1}${p2}${p3}${p4}${p5}${p6}`;
